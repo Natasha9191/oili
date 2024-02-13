6f338b976054db0538dca0c630af36c752bb6ec7
Natasha9191 <133143274+Natasha9191@users.noreply.github.com>
2024-02-13 23:54:46 +0530

    first commit

---
+++ autoclipping.com/content/jslocale/en5572.js
@@ -0,0 +1,40 @@
+window.TRANSLATIONS = TRANSLATIONS = {
+    "auth": {
+        "Not found": null,
+        "Notice \"passwordreminder\" is not defined": "",
+        "Router \"account\" not defined": "",
+        "Some of your log in data is incorrect": null,
+        "Success": "Success",
+        "This account does not exist": "This account does not exist",
+        "empty_email": "Please fill in your email address",
+        "empty_password": "Please fill in your password",
+        "invalid_access": "Some of your log in data is incorrect",
+        "invalid_data": "A user with the specified e-mail already exists",
+        "invalid_email": "Incorrect email address",
+        "invalid_password_confirm": "Passwords do not match ",
+        "invalid_password_length": "Password must be at least 8 symbols long",
+        "invalid_password_numbers": "Password must include numbers",
+        "invalid_password_uppercase": "Password must have upper case letters",
+        "not_found": "Not found",
+        "test": null
+    },
+    "default": {
+        "Mark some pictures as favorites to see them here": "Mark some pictures as favorites to see them here",
+        "Success": "Success",
+        "YOU HAVEN'T UPLOADED ANY IMAGES YET": "YOU HAVEN'T UPLOADED ANY IMAGES YET",
+        "You don't have any deleted files yet": "You don't have any deleted files yet",
+        "You haven't downloaded any images yet": "You haven't downloaded any images yet",
+        "filter_group_added": "Added",
+        "filter_group_all": "All",
+        "filter_group_modified": "Modified",
+        "filter_group_size": "Size"
+    },
+    "lightbox": {"Image": null},
+    "payment_errors": {
+        "OK_PAYG": "Thank You. Your AutoClipping.com payment %price% was successful and %credits% credits have been added to your account.",
+        "OK_SUB": "Thank You. Your AutoClipping.com subscription %price% per month is active now and %credits% credits have been added to your account.",
+        "INVALID_CVC": "Unfortunately Your AutoClipping.com payment %price% was NOT successful because Your card CVC (3 or 4 digit code on the back of the card) didn’t match details registered in Your Bank. Please try again.",
+        "INVALID_EXPIRY_DATE": "Unfortunately Your AutoClipping.com payment %price% was NOT successful because Your card Expiry date was wrong. Please check the details and try again.",
+        "CARD_DECLINED": "Unfortunately Your AutoClipping.com payment %price% was NOT successful. Potential reason: No Funds on the card. Please fund Your card and try again."
+    }
+};
